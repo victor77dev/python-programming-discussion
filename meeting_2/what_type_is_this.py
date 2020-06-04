b4204196cc5452dff63b311d6ef412677a4af7df
victor77dev <victor77.dev@gmail.com>
2020-06-04 23:42:00 +0200

    Add what_type_is_this for Meeting 2

---
+++ meeting_2/what_type_is_this.py
@@ -0,0 +1,23 @@
+def what_type_is_this(variable):
+    # Let's try debugger
+    print(f'{variable} is ', end='')
+    print(f'{type(variable)}')
+
+what_type_is_this(42)
+what_type_is_this(3.14)
+what_type_is_this(42.)
+what_type_is_this(1/3)
+what_type_is_this(1//3)
+what_type_is_this('Coding')
+what_type_is_this('Don\'t panic')
+what_type_is_this(1/3*3 == 1)
+what_type_is_this(True)
+what_type_is_this(1 == 1 + 3 or True)
+a = -1
+what_type_is_this(a)
+what_type_is_this(abs(a))
+a = 'Haha'
+what_type_is_this(a)
+a = ['Haha', 1]
+what_type_is_this(a)
+what_type_is_this(abs(a))
