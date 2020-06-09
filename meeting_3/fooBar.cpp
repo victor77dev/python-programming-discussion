cd31fdc361384233069caeaa54961eee45dbe812
victor77dev <victor77.dev@gmail.com>
2020-06-10 00:24:02 +0200

    Add FooBar c++ example

---
+++ meeting_3/fooBar.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void fooBar(int num);
+
+int main() {    
+    fooBar(15);
+    fooBar(42);
+    fooBar(100);
+}
+
+void fooBar(int num) {
+    cout << "Foo Bar Game of " << num << endl;
+    for (int i = 1; i <= num; i++) {
+        if (i % 3 == 0) cout << "Foo";
+        if (i % 5 == 0) cout << "Bar";
+        if (i % 3 != 0 && i % 5 != 0) cout << i;
+        cout << endl;
+    }
+}
