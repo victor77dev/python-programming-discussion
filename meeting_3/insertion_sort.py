848089deff1bf5ac18d9de8ab595b95beee19b3b
victor77dev <victor77.dev@gmail.com>
2020-06-11 21:30:49 +0200

    Fix style

--- meeting_3/insertion_sort.py
+++ meeting_3/insertion_sort.py
@@ -17,12 +17,13 @@ def insertion_sort(list):
 
 
 list = [21, 5, 22, 20, 12, 3, 11, 5, 13, 16]
-list2 = [1, 2, 30, 4, 5, 6, 12, 20, 25, 30]
 print(len(list), insertion_sort(list))
-print(len(list2), insertion_sort(list2))
 print(list)
-print(list2)
+
+list = [1, 2, 30, 4, 5, 6, 12, 20, 25, 30]
+print(len(list), insertion_sort(list))
+print(list)
 
 list.reverse()
-print(len(list2), insertion_sort(list2))
-print(list2)
+print(len(list), insertion_sort(list))
+print(list)
