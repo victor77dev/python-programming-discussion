ee086beecef0742d7964a8dbb73aa4d8f7264e13
Steven <valcalstart@gmail.com>
2020-06-27 09:55:07 +0200

    Homework-steven

--- meeting_2/what_type_is_this.py
+++ meeting_2/what_type_is_this.py
@@ -5,18 +5,18 @@ def what_type_is_this(variable):
     print(f'{type(variable).__name__}')
 what_type_is_this.line = 7
 
-what_type_is_this(42)
-what_type_is_this(3.14)
-what_type_is_this(42.)
-what_type_is_this(1/3)
-what_type_is_this(1//3)
-what_type_is_this(2//3)
-what_type_is_this('Coding')
-what_type_is_this('Don\'t panic')
-what_type_is_this(1/3*3 == 1)
-what_type_is_this(True)
-what_type_is_this(0 == True)
-what_type_is_this(0 == False)
+what_type_is_this(42) #integer
+what_type_is_this(3.14) #float
+what_type_is_this(42.) #float
+what_type_is_this(1/3) #float
+what_type_is_this(1//3) #integer
+what_type_is_this(2//3) #integer
+what_type_is_this('Coding') #string
+what_type_is_this('Don\'t panic') #string
+what_type_is_this(1/3*3 == 1) # true, boolean
+what_type_is_this(True) #true, boolean
+what_type_is_this(0 == True) # false, boolean
+what_type_is_this(0 == False) #true, boolean
 what_type_is_this(1 == True)
 what_type_is_this(1 == False)
 what_type_is_this(2 == True)
