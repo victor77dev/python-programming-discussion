c72a1f28cedf3afbad99a701bb850c35809b5cd6
victor77dev <victor77.dev@gmail.com>
2020-07-08 21:00:11 +0200

    Add Insertion sort code from Heitke for code review (#8)
    * Add Insertion sort code from Heitke
    
    * Update code-for-review/insertion sort.py
    
    * Update code-for-review/insertion sort.py
    
    Co-authored-by: jjiajian <66494256+jjiajian@users.noreply.github.com>
    
    * Update code-for-review/insertion sort.py
    
    * Update code-for-review/insertion sort.py
    
    * Fix style
    
    Co-authored-by: jjiajian <66494256+jjiajian@users.noreply.github.com>

---
+++ code-for-review/insertion sort.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 17:19:43 2020
+
+@author: heitk
+"""
+
+list = [21, 5, 22, 20, 12, 3, 11, 5, 13, 16]
+
+def insertion_help(list, ele):
+    i = 0
+    if len(list) == 0:
+        list.insert(i, ele)
+    else:
+        while list[i]<ele:
+            i = i + 1
+            if i == len(list):
+                list.insert(i + 1, ele)
+                return list
+
+        list.insert(i, ele)
+    return list
+
+insertion_help(list, 7)
+
+
+
+
+def insertion_sort(list):
+    new_list = []
+
+    for ele in list:
+        new_list = insertion_help(new_list, ele)
+
+    return new_list
+
+
+print (list)
+print (insertion_sort(list))
